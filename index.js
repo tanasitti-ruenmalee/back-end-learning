abf01cfa248f139a0a97b9afa685272e0899359f
ปัทมราช บูรณวนิช <pattamaraj.boo@sbac.ac.th>
2026-01-28 15:40:00 +0700

    v2

--- index.js
+++ index.js
@@ -2,8 +2,27 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
+app.use(express.json());
+
+let department = [
+  { id: 1, name: "IT" },
+  { id: 2, name: "DBT" },
+];
+
 app.get("/", (req, res) => {
   res.send("Mr.tanasitiit Ruenmalee");
+});
+
+app.get("/department", (req, res) => {
+  res.send(department);
+});
+app.post("/department", (req, res) => {
+  let newDepartment = {
+    id: department.length + 1,
+    name: req.body.name,
+  };
+  department.push(newDepartment);
+  res.send(newDepartment);
 });
 
 app.listen(port, () => {
